18e2f971836be9108264fa61fd7d86db1839c2ce
OGNova <jamooney2000@gmail.com>
2017-11-30 12:21:24 -0500

    Fix double quotes

--- commands/Moderation/purge.js
+++ commands/Moderation/purge.js
@@ -1,23 +1,23 @@
-const Moderation = require("../../base/Moderation.js");
+const Moderation = require('../../base/Moderation.js');
 
 class Purge extends Moderation {
   constructor(client) {
     super(client, {
-      name: "purge",
-      description: "It purges between 2 and 99 messages.",
-      usage: "purge [user] <number>",
-      extended: "This command will either purge a mentioned users messages (between 2 and 99), or the bots own messages.",
-      aliases: ["prune"],
-      botPerms: ["SEND_MESSAGES", "MANAGE_MESSAGES"],
-      permLevel: "Moderator"
+      name: 'purge',
+      description: 'It purges between 2 and 99 messages.',
+      usage: 'purge [user] <number>',
+      extended: 'This command will either purge a mentioned users messages (between 2 and 99), or the bots own messages.',
+      aliases: ['prune'],
+      botPerms: ['SEND_MESSAGES', 'MANAGE_MESSAGES'],
+      permLevel: 'Moderator'
     });
   }
 
   async run(message, args, level) { // eslint-disable-line no-unused-vars
     const user = message.mentions.users.first();
-    const amount = parseInt(message.content.split(" ")[1]) ? parseInt(message.content.split(" ")[1]) : parseInt(message.content.split(" ")[2]);
-    if (!amount) return message.reply("Must specify an amount to delete!");
-    if (!amount && !user) return message.reply("Must specify a user and amount, or just an amount, of messages to purge!");
+    const amount = parseInt(message.content.split(' ')[1]) ? parseInt(message.content.split(' ')[1]) : parseInt(message.content.split(' ')[2]);
+    if (!amount) return message.reply('Must specify an amount to delete!');
+    if (!amount && !user) return message.reply('Must specify a user and amount, or just an amount, of messages to purge!');
     message.channel.fetchMessages({ limit: amount }).then((messages) => {
       if (user) {
         const filterBy = user ? user.id : this.client.user.id;
