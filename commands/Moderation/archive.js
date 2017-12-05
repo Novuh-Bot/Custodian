3dd43a3a92f3c4a7392d334fd81be18b1a0a4ea6
OGNova <jamooney2000@gmail.com>
2017-12-05 14:27:25 -0500

    Archive Command

--- commands/Moderation/archive.js
+++ commands/Moderation/archive.js
@@ -17,15 +17,15 @@ class Archive extends Command {
   async run(message, args, level) {
     const amount = args[0];
     try {
-      const msgs = message.channel.fetchMessages({ limit: `${amount}`})
+      const msgs = await message.channel.fetchMessages({ limit: `${amount}`})
         .then(messages => messages.map(m => `${m.createdAt} (${m.guild.id} / #${m.channel.id} / ${m.author.id}) ${m.author.tag} : ${m.cleanContent}`).join('\n'));
       const hasteURL = await require('snekfetch')
         .post('https://hastebin.com/documents')
         .send(msgs).catch(e => {
           throw new Error(`Error posting data: ${e}`);
         });
-      const url = `http://hastebin.com/${hasteURL.body.key}.txt`;
-      message.channel.send(`Here's the archive. ${url}`);
+      const archive = `http://hastebin.com/${hasteURL.body.key}.txt`;
+      message.channel.send(`Here's the archive. ${archive}`);
     } catch (error) {
       throw error;
     }
