237643ed08094afbc73b331c25387e055e89a4eb
OGNova <jamooney2000@gmail.com>
2017-12-21 23:09:35 -0500

    Minor Fixes

--- monitors/monitor.js
+++ monitors/monitor.js
@@ -8,8 +8,7 @@ function giveRandomPoints(min, max) {
 module.exports = class {
 
   static run(client, message, level) {
-    this.givePoints(client, message, level),
-    this.nms = require('./nms.js').run(client, message, level);
+    this.givePoints(client, message, level);
   }
 
   static givePoints(client, message, level) {
