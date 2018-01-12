3b0c4478aa1037b9ac97eb58ca5684c0d699f9df
OG Novuh <jamooney2000@gmail.com>
2018-01-11 19:05:55 -0500

    Category fixes 2.

--- commands/System/stats.js
+++ commands/System/stats.js
@@ -9,7 +9,7 @@ class Stats extends Command {
       name: 'stats',
       description: 'Gives some useful bot statistics',
       usage: 'stats',
-      category: 'Utilities',
+      category: 'System',
       botPerms: ['SEND_MESSAGES'],
       permLevel: 'User'
     });
