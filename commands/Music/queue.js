5ffc856b85d93349a25c481dc6ef0fa19d8f90e3
OG Novuh <jamooney2000@gmail.com>
2018-01-20 20:56:03 -0500

    Music Features!

--- commands/Music/queue.js
+++ commands/Music/queue.js
@@ -1,4 +1,6 @@
 const Command = require('../../base/Command.js');
+const { RichEmbed } = require('discord.js');
+const embedCheck = require('../../modules/embedPerms.js');
 
 class Queue extends Command {
   constructor(client) {
@@ -13,9 +15,32 @@ class Queue extends Command {
   }
 
   async run(message, args, level) {
-    message.channel.send('Coming soon!');
-    const playlists = this.client.playlists.get(message.guild.id);
-    playlists.map().join('➢\n');
+    const settings = this.client.getSettings(message.guild.id);
+    if (!this.client.playlists.has(message.guild.id)) return message.channel.send(`The queue is empty. Add some songs first with ${settings.prefix}play <song name>`);
+
+    let playlist = this.client.playlists.get(message.guild.id);
+    playlist = playlist.queue.slice(playlist.position);
+
+    const current = playlist.shift();
+    const singular = playlist.length === 1;
+    const embed = new RichEmbed()
+      .setTitle(`Currently playing **${current.songTitle.substring(0, 50)}** (${current.playTime})`)
+      .setColor(0xDD2825)
+      .setFooter(`Requested by ${current.requester}`, current.requesterIcon)
+      .setDescription(`There ${singular ? 'is' : 'are'} currently ${playlist.length} song${singular ? '' : 's'} in the queue.\n`)
+      .setThumbnail()
+      .setTimestamp()
+      .setURL(current.url);
+
+    if (embedCheck(message)) {
+      for (let i = 0; i < playlist.length && i < 5; i++) {
+        embed.addField(`🎧 ${playlist[i].songTitle.substring(0, 50)} (${playlist[i].playTime})`, `🤘 Requested by **${playlist[i].requester}**`);
+      }
+      message.channel.send({embed});
+    } else {
+      message.channel.send(`Currently playing **${current.songTitle.substring(0, 50)}** (${current.playTime})\n\nThere ${singular ? 'is' : 'are'} currently ${playlist.length} song${singular ? '' : 's'} in the queue.\n${playlist.map.size === 0 ? '' : '🎧' + playlist.map(i => '_' + i.songTitle+'_ (' + i.playTime + ') requested by **' + i.requester + '**\n🔗 <https://www.youtube.com/watch?v='+i.id+'>\n').join('\n🎧 ')}`);
+    }
+
   }
 }
 
