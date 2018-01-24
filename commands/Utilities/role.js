6ef22c0c7c346fe56393e36d40c989cd778e2032
OG Novuh <jamooney2000@gmail.com>
2018-01-23 19:40:00 -0500

    Role command, from the guide I wrote!

--- commands/Utilities/role.js
+++ commands/Utilities/role.js
@@ -4,19 +4,46 @@ class Role extends Command {
   constructor(client) {
     super(client, {
       name: 'role',
-      description: 'Gives/Takes a role from you.',
+      description: 'Give or take a role to/from a user',
       category: 'Utilities',
-      usage: 'role -g|-t|-l|-a <role id>',
+      usage: 'role <-give|-take> <role name',
       guildOnly: true,
-      extended: 'This command allows you to add roles to a list of self-assignable roles. Flags are -g, -t, -l, and -a. They will give you the role, take the role, list the self-assignable roles, or add a role to the list. Adding a role requires the moderator role.',
       botPerms: ['SEND_MESSAGES'],
-      permLevel: 'User'
+      permLevel: 'Moderator'
     });
   }
 
   async run(message, args, level) {
-    const settings = this.client.settings.get(message.guild.id);
-    message.channel.send('Coming soon!'); 
+    switch (message.flags[0]) {
+      case 'add': {
+        if (message.mentions.members.size === 0) return message.reply('Please mention a user to give the role to.');
+        const member = message.mentions.members.first();
+        const name = args.slice(1).join(' ');
+        const role = message.guild.roles.find('name', name);
+        if (!role) return message.reply('I can\'t seem to find that role.');
+        try {
+          await member.addRole(role);
+          await message.channel.send(`I've added the ${name} role to ${member.dsiplayName}.`);
+        } catch (e) {
+          throw e;
+        }
+        break;
+      }
+  
+      case 'remove': {
+        if (message.mentions.members.size === 0) return message.reply('Please mention a user to take the role from.');
+        const member = message.mentions.members.first();
+        const name = args.slice(1).join(' ');
+        const role = message.guild.roles.find('name', name);
+        if (!role) return message.reply('I can\'t seem to find that role.');
+        try {
+          await member.removeRole(role);
+          await message.channel.send(`I've removed the ${name} role from ${member.displayName}.`);
+        } catch (e) {
+          throw e;
+        }
+      }
+    }
   }
 }
 
