11d7f9619bda533e329db45226eb451e8a25018c
OG Novuh <jamooney2000@gmail.com>
2018-01-25 13:53:41 -0500

    Too small.

--- commands/Fun/expprof.js
+++ commands/Fun/expprof.js
@@ -4,7 +4,7 @@ const { Canvas } = require('canvas-constructor');
 
 const buildProfile = async (person) => {
   console.log(`${person}`);
-  const png = person.replace(/\.(gif|jpg|png|jpeg)/, '.png');
+  const png = person.replace(/\.(gif|jpg|png|jpeg)/, '.png?size=256');
   console.log(`${png}`);
   const { body } = await snek.get(png);
   return await new Canvas(405, 720)
