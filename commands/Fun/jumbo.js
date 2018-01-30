26bc17efe7381a535b02697b5202acfa44eaae20
OG Novuh <jamooney2000@gmail.com>
2018-01-29 19:34:46 -0500

    Well son of a bitch.

--- commands/Fun/jumbo.js
+++ commands/Fun/jumbo.js
@@ -1,6 +1,7 @@
 const Command = require('../../base/Command.js');
 const discord = require('discord.js');
 const snek = require('snekfetch');
+const twemoji = require('twemoji');
 const fs = require('fs');
 
 class Jumbo extends Command {
@@ -20,14 +21,19 @@ class Jumbo extends Command {
       const emote = discord.Util.parseEmoji(args[0]);
       if (emote.animated === true) {
         const URL = `https://cdn.discordapp.com/emojis/${emote.id}.gif?v=1`;
-        const { body } = await snek.get(`${URL}`);
-        const buffer = body;
+        const { body: buffer } = await snek.get(`${URL}`);
         const toSend = fs.writeFileSync('emote.gif', buffer);
         message.channel.send({ file: 'emote.gif' });
+      } else if (emote.id === null) {
+        const twemote = twemoji.parse(args[0]);
+        const regex = /src="(.+)"/g;
+        const regTwemote = regex.exec(twemote)[1];
+        const { body: buffer } = await snek.get(`${regTwemote}`);
+        const toSend = fs.writeFileSync('emote.png', buffer);
+        await message.channel.send({ file: 'emote.png' });
       } else {
         const URL = `https://cdn.discordapp.com/emojis/${emote.id}.png`;
-        const { body } = await snek.get(`${URL}`);
-        const buffer = body;
+        const { body: buffer } = await snek.get(`${URL}`);
         const toSend = fs.writeFileSync('emote.png', buffer);
         message.channel.send({ file: 'emote.png' });
       }
