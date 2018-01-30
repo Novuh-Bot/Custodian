de9ec7658556ea6eded763d7b3af1706b2374d99
OG Novuh <jamooney2000@gmail.com>
2018-01-29 22:38:21 -0500

    Update message* events.

--- events/messageDelete.js
+++ events/messageDelete.js
@@ -11,14 +11,6 @@ module.exports = class {
     if (!message || !message.id || !message.content || !message.guild) return;
     const channel = message.guild.channels.find('name', 'raw-logs');
     if (!channel) return;
-    channel.send(`\`[${moment(message.createdAt).format('h:mm:ss')}]\` 🗑 ${message.author.tag} (${message.author.id}) : Message Deleted in ${message.channel.name}:\n${message.cleanContent}`);
-    
-    const settings = this.client.settings.get(message.guild.id);
-    const Deletion = new RichEmbed()
-      .setColor(0x00AE86)
-      .setTimestamp()
-      .setDescription(`**Action:** Message Deletion\n**Message Author:** ${message.author.username}#${message.author.discriminator}\n**Message Content:** ${message}`);
-    if (settings.extensiveLogging !== 'true') return;
-    message.guild.channels.find('name', settings.modLogChannel).send({ embed: Deletion });
+    channel.send(`\`[${moment(message.createdAt).format('h:mm:ss')}]\` 🗑 ${message.author.tag} (\`${message.author.id}\`) Message Deleted in **#${message.channel.name}**:\n${message.cleanContent}`);
   }
 };
--- events/messageUpdate.js
+++ events/messageUpdate.js
@@ -22,14 +22,6 @@ module.exports = class {
     if (!oldMessage || !oldMessage.id || !oldMessage.content || !oldMessage.guild) return;
     const channel = oldMessage.guild.channels.find('name', 'raw-logs');
     if (!channel) return;
-    channel.send(`\`[${moment(oldMessage.createdAt).format('h:mm:ss')}]\` 📝 ${oldMessage.author.tag} (${oldMessage.author.id}) : Message Edited in ${oldMessage.channel.name}:\n**B**: ${oldMessage.cleanContent}\n**N**: ${newMessage.cleanContent}`);
-
-    const settings = this.client.settings.get(oldMessage.guild.id);
-    const Edit = new RichEmbed()
-      .setColor(0x00AE86)
-      .setTimestamp()
-      .setDescription(`**Action:** Message Edited\n**Message Author:** ${oldMessage.author}\n**Old Content:** ${oldMessage.content}\n**New Content:** ${newMessage.content}`);
-    if (settings.extensiveLogging !== 'true') return;
-    oldMessage.guild.channels.find('name', settings.modLogChannel).send({ embed: Edit });
+    channel.send(`\`[${moment(oldMessage.createdAt).format('h:mm:ss')}]\` 📝 ${oldMessage.author.tag} (\`${oldMessage.author.id}\`) Message Edited in **#${oldMessage.channel.name}**:\n**B**: ${oldMessage.cleanContent}\n**N**: ${newMessage.cleanContent}`);
   }
 };
