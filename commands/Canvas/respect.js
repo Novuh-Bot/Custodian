787e5cb868001d09dbb8f94e8a8f7d9612fe4ac3
OG Novuh <jamooney2000@gmail.com>
2018-02-03 15:33:09 -0500

    The one where I update respect.

--- commands/Canvas/respect.js
+++ commands/Canvas/respect.js
@@ -1,7 +1,6 @@
 const Social = require('../../base/Social.js');
 const snek = require('snekfetch');
 const { Attachment } = require('discord.js');
-const { yorkAPIKey } = require('../../config.js');
 
 class Respect extends Social {
   constructor(client) {
@@ -18,10 +17,6 @@ class Respect extends Social {
 
   async run(message, args, level) {
     try {
-      // const person = message.mentions.users.first() || message.member;
-      // const { body } = await snek.get(`http://api.anidiots.guide/api/respect/?avatar=${person.displayAvatarURL}`).set('token', `${yorkAPIKey}`);
-      // await message.channel.send('Press F to pay respects', { files: [{ attachment: body, name: 'respects.png' }] });
-      
       await message.channel.send(new Attachment(await this.client.api.respect((message.mentions.users.first() || message.author).displayAvatarURL), 'respect.png'));
     } catch (error) {
       throw error;
