504549d898b05b3ece7bb304845b7bda4234c3b6
OG Novuh <jamooney2000@gmail.com>
2018-02-06 14:19:27 -0500

    Update eval.

--- commands/System/eval.js
+++ commands/System/eval.js
@@ -1,36 +1,65 @@
 const Command = require('../../base/Command.js');
 const Stopwatch = require('../../util/Stopwatch.js');
+const { inspect } = require('util');
+const { post } = require('snekfetch');
 
 class Eval extends Command {
   constructor(client) {
     super(client, {
       name: 'eval',
       description: 'Evaluates arbitrary Javascript.',
-      category:'System',
+      category: 'System',
       usage: 'eval <expression:string>',
+      extended: 'This is an extremely dangerous command, use with caution and never eval stuff strangers tell you.',
       aliases: ['ev'],
-      permLevel: 'Bot Owner'
+      permLevel: 'Bot Admin'
     });
   }
 
-  async run(message, [code], level) { // eslint-disable-line no-unused-vars     
+  async run(message, args, level) { // eslint-disable-line no-unused-vars
     const stopwatch = new Stopwatch();
     let syncTime, asyncTime;
+    const { client } = message;
+    const code = args.join(' ');
+    const token = client.token.split('').join('[^]{0,2}');
+    const rev = client.token.split('').reverse().join('[^]{0,2}');
+    const filter = new RegExp(`${token}|${rev}`, 'g');
     try {
-      const evaled = eval(code);
-      const clean = await this.client.clean(this.client, evaled);
+      let output = eval(code);
       syncTime = stopwatch.friendlyDuration;
-      const msg = await message.channel.send('Evaling.');
-      stopwatch.stop();
-      const time = this.formatTime(syncTime, asyncTime);
-      msg.edit(`\`\`\`js\n${clean}\n\`\`\`\n\n${time}`);
-    } catch (err) {
-      message.channel.send(`\`ERROR\` \`\`\`xl\n${await this.client.clean(this.client, err)}\n\`\`\``);
+      if (output instanceof Promise || (Boolean(output) && typeof output.then === 'function' && typeof output.catch === 'function')) {
+        stopwatch.restart();
+        output = await output;
+        asyncTime = stopwatch.friendlyDuration;
+      }
+      output = inspect(output, { depth: 0, maxArrayLength: null });
+      output = output.replace(filter, '[TOKEN]');
+      output = this.clean(output);
+      if (output.length < 1950) {
+        stopwatch.stop();
+        const time = this.formatTime(syncTime, asyncTime);
+        message.channel.send(`\`\`\`js\n${output}\n\`\`\`\n\n${time}`);
+      } else {
+        try {
+          const { body } = await post('https://www.hastebin.com/documents').send(output);
+          message.channel.send(`Output was to long so it was uploaded to hastebin https://www.hastebin.com/${body.key}.js `);
+        } catch (error) {
+          message.channel.send(`I tried to upload the output to hastebin but encountered this error ${error.name}:${error.message}`);
+        }
+      }
+    } catch (error) {
+      message.channel.send(`The following error occured \`\`\`js\n${error.stack}\`\`\``);
     }
   }
 
   formatTime(syncTime, asyncTime) {
     return asyncTime ? `⏱ ${asyncTime}<${syncTime}>` : `⏱ ${syncTime}`;
+  }
+
+  clean(text)  {
+    return text
+      .replace(/`/g, '`' + String.fromCharCode(8203))
+      .replace(/@/g, '@' + String.fromCharCode(8203));
   }
 }
 
