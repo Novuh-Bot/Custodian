64403b29b65504ab0fd3fb7ae5887ea6cfdbf35f
OG Novuh <jamooney2000@gmail.com>
2018-02-05 20:09:21 -0500

    99 bugs in the code, 99 bugs in the code. Patch one up, 105 bugs in the code.

--- commands/Canvas/thesearch.js
+++ commands/Canvas/thesearch.js
@@ -15,8 +15,7 @@ class Thesearch extends Command {
   async run(message, args, level) {
     if (message.mentions.users.size === 0) {
       const text = args.join(' ');
-      if (text.length < 1) return message.channel.send('You must give an achievement description.');
-      if (text.length > 22) return message.channel.send('I can only handle a maximum of 22 characters');
+      if (text.length < 1) return message.channel.send('You must supply something for the guy to say.');
       try {
         await message.channel.send(new Attachment(await this.client.api.theSearch((message.mentions.users.first() || message.author).displayAvatarURL, text), 'achievement.png'));
       } catch (error) {
@@ -24,8 +23,7 @@ class Thesearch extends Command {
       }
     } else {
       const text = args.slice(1).join(' ');
-      if (text.length < 1) return message.channel.send('You must give an achievement description.');
-      if (text.length > 22) return message.channel.send('I can only handle a maximum of 22 characters');
+      if (text.length < 1) return message.channel.send('You must supply something for the guy to say.');
       try {
         await message.channel.send(new Attachment(await this.client.api.theSearch((message.mentions.users.first() || message.author).displayAvatarURL, text), 'achievement.png'));
       } catch (error) {
