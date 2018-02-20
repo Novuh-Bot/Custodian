493bcf6332e3a1f451f7e7c1563776f16c4ebdae
OGNova <jamooney2000@gmail.com>
2018-02-19 19:15:05 -0500

    Update index and package for proper node version.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-if (process.version.slice(1).split('.')[0] < 8) throw new Error('Node 8.0.0 or higher is required. Update Node on your system.');
+if (process.version.slice(1).split('.')[1] < 5) throw new Error('Node 8.5.0 or higher is required. Update Node on your system.');
 
 const Discord = require('discord.js');
 const { promisify } = require('util');
