87d51d1df685ba04c73432b9dc2a23890668b56c
OGNova <jamooney2000@gmail.com>
2018-02-21 10:57:48 -0500

    Fix CI failure.

--- commands/Moderation/lockdown.js
+++ commands/Moderation/lockdown.js
@@ -19,7 +19,7 @@ class Lockdown extends Moderation {
     const { lang } = this.client.settings.get(message.guild.id);
 
     const time = args.join(' ');
-    if (!time) return message.lang(message, lang, this.help.category, 'lockdownNoTime')
+    if (!time) return message.lang(message, lang, this.help.category, 'lockdownNoTime');
     message.channel.lock(this.client, message, time);
   }
 }
