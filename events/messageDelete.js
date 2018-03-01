d82d8b36f18039e70de6d65b1b89b52735163421
OGNova <jamooney2000@gmail.com>
2018-02-28 20:04:50 -0500

    Spelling error.

--- events/messageDelete.js
+++ events/messageDelete.js
@@ -10,7 +10,7 @@ module.exports = class {
   async run(message, client) {
 
     if (config.ignoredUsers.includes(message.author.id)) return;
-    if (config.ignoredChannel.includes(message.channel.id)) return;
+    if (config.ignoredChannels.includes(message.channel.id)) return;
     
     if (!message || !message.id || !message.content || !message.guild) return;
     const channel = message.guild.channels.find('name', 'raw-logs');
