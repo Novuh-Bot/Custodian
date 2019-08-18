5204e962c52805e37d741fe3a8a2eef92e46f41c
OGNova <jamooney2000@gmail.com>
2019-08-17 20:33:46 -0400

    Errors man, they're annoying.

--- index.js
+++ index.js
@@ -10,4 +10,13 @@ if (process.env.NODE_ENV === 'development') {
 
 process.on('unhandledRejection', err => {
   console.error(`Uncaught Promise Error: ${err}`);
-}); 
+});
+
+process.on('uncaughtException', (err) => {
+  const errorMsg = err.stack.replace(new RegExp(`${__dirname}/`, 'g'), './');
+  client.logger.error(`Uncaught Exception: ${errorMsg}`);
+  console.error(err);
+  // Always best practice to let the code crash on uncaught exceptions. 
+  // Because you should be catching them anyway.
+  process.exit(1);
+});
