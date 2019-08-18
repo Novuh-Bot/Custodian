80c2fdd68df59f58359d268bb8c912d1fc6d3707
OGNova <jamooney2000@gmail.com>
2019-08-17 20:09:36 -0400

    Create logger in development environment.

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const { init, createLogger } = require('./src/lib/Client.js');
 
 if (process.env.NODE_ENV === 'development') {
   init(process.env.DEV_TOKEN).catch(e => console.log(e));
+  createLogger();
 } else {
   init(process.env.PROD_TOKEN).catch(e => console.log(e));
   createLogger();
